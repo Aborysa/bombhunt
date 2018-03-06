6c073a3edb85e2fca946797d558b8df62a6cc3f9
Aborysa <aborysa@gmail.com>
2018-03-06 07:28:01 +0100

    Setup update, rendering and temporary viewswitching

--- game/core/src/com/bombhunt/game/BombHunt.java
+++ game/core/src/com/bombhunt/game/BombHunt.java
@@ -2,32 +2,44 @@ package com.bombhunt.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.bombhunt.game.view.GameScreen;
+import com.bombhunt.game.view.IView;
 
 public class BombHunt extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	IView currentView;
+
+	public void setCurrentView(IView view){
+		// May want to despose old view
+		Gdx.input.setInputProcessor(view.getInputProcessor());
+		currentView = view;
+	}
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setCurrentView(new GameScreen());
+
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		float dtime = Gdx.graphics.getDeltaTime();
+
+		currentView.update(dtime);
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		currentView.render();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
