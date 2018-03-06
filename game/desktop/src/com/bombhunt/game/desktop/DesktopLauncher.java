7376ad9e3217528771e8ac1b92955da6be18e201
Aborysa <aborysa@gmail.com>
2018-03-06 06:32:32 +0100

    Setup java libgdx project

---
+++ game/desktop/src/com/bombhunt/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bombhunt.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bombhunt.game.BombHunt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BombHunt(), config);
+	}
+}
