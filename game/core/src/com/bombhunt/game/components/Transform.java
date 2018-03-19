ee6a68aad90d9a2cb97ec50bbe5837fc62c902bd
Jarle Trollebø <aborysa@gmail.com>
2018-03-19 09:20:03 +0100

    Changed from floats to Vectors

--- game/core/src/com/bombhunt/game/components/Transform.java
+++ game/core/src/com/bombhunt/game/components/Transform.java
@@ -1,6 +1,7 @@
 package com.bombhunt.game.components;
 
 import com.artemis.Component;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -11,38 +12,23 @@ import com.badlogic.gdx.math.Vector2;
 public class Transform extends Component{
 
     // physical position of entity
-    public float posX;
-    public float posY;
-
-    // render layer
-    public float posZ;
+    // depth = position.z
+    public Vector3 position;
 
     // physical rotation of entity
     public float rotation;
 
-    public float scaleX;
-    public float scaleY;
+    public Vector2 scale;
 
     // adding this component to entity, can either specify where to add it
-    public Transform(float posX, float posY, float posZ, float scaleX, float scaleY, float rotation){
-        this.posX = posX;
-        this.posY = posY;
-        this.posZ = posZ;
-        this.scaleX = scaleX;
-        this.scaleY = scaleY;
+    public Transform(Vector3 position, Vector2 scale, float rotation){
+        this.position = position.cpy();
+        this.scale = scale.cpy();
         this.rotation = rotation;
     }
 
     public Transform(){
-        this(0, 0, 0, 1, 1, 0);
+        this(Vector3.Zero, new Vector2(1f, 1f), 0);
     }
 
-    // choose either this or the next function for the game, can be used to update positions for enemy AI when recieving position data.
-    /*public void setPosition(float posX, float posY){
-
-    }
-
-    public void setPosition(Vector2 position){
-
-    }*/
 }
