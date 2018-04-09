acf7d99793b48f313505c6d329a336d7dfd2b889
Aborysa <aborysa@gmail.com>
2018-04-09 02:24:28 +0200

    Created ecs grid system

---
+++ game/core/src/com/bombhunt/game/model/ecs/components/GridPositionComponent.java
@@ -0,0 +1,14 @@
+package com.bombhunt.game.model.ecs.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.math.Vector2;
+import com.bombhunt.game.model.Grid;
+
+
+public class GridPositionComponent extends Component {
+  public Grid grid;
+  public boolean snapToGrid = true;
+  public boolean accumelate = true;
+  public Vector2 accumelator = new Vector2();
+  public int cellIndex = -1;
+}
