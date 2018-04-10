76a412969026e40827b8c445546bf5aaa5bd15e7
Jarle Trollebø <aborysa@gmail.com>
2018-04-10 12:15:48 +0200

    Replaced Math.floordiv with division, not supported in android sdk <= 19. Fixed intendation

--- game/core/src/com/bombhunt/game/model/Grid.java
+++ game/core/src/com/bombhunt/game/model/Grid.java
@@ -5,72 +5,72 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Grid {
 
-  private int cellSize;
-  private int width, height;
+    private int cellSize;
+    private int width, height;
 
-  private IntBag[] gridCells;
+    private IntBag[] gridCells;
 
+    public Grid(int width, int height, int cellSize) {
+        this.cellSize = cellSize;
+        this.width = width;
+        this.height = height;
 
-  public Grid(int width, int height, int cellSize) {
-    this.cellSize = cellSize;
-    this.width = width;
-    this.height = height;
-
-    this.gridCells = new IntBag[width * height];
-    for(int i = 0; i < this.gridCells.length; i++) {
-      this.gridCells[i] = new IntBag(10);
+        this.gridCells = new IntBag[width * height];
+        for(int i = 0; i < this.gridCells.length; i++) {
+            this.gridCells[i] = new IntBag(10);
+        }
     }
-  }
 
 
-  public Vector2 getSnappedPosition(Vector2 position) {
-    Vector2 snapped = position.cpy();
-    snapped.x = Math.floorDiv((int)snapped.x, cellSize);
-    snapped.y = Math.floorDiv((int)snapped.y, cellSize);
-    snapped.scl(cellSize);
-    return snapped;
-  }
+    public Vector2 getSnappedPosition(Vector2 position) {
+        Vector2 snapped = position.cpy();
 
-  public boolean isOutOfBounds(int x, int y){
-    return x < 0 || x >= width || y < 0 || y >= height;
-  }
-
-  public boolean isOutOfBounds(int index){
-    return index < 0 || index >= gridCells.length;
-  }
-
-  public int getCellIndex(int x, int y){
-    if(isOutOfBounds(x, y)) {
-      return -1;
+        snapped.x = (int) (snapped.x / cellSize);
+        snapped.y = (int) (snapped.y / cellSize);
+        snapped.scl(cellSize);
+        return snapped;
     }
-    return x + y * width;
-  }
 
-  public int getCellIndex(Vector2 position){
-    return getCellIndex(Math.floorDiv((int)position.x, cellSize),Math.floorDiv((int)position.y, cellSize));
-  }
-
-  public IntBag getEntities(int index) {
-    try{
-      return this.gridCells[index];
-    }catch(IndexOutOfBoundsException e) {
-      return null;
+    public boolean isOutOfBounds(int x, int y){
+        return x < 0 || x >= width || y < 0 || y >= height;
     }
-  }
 
-  public IntBag getEntities(int x, int y) {
-    return getEntities(getCellIndex(x, y));
-  }
+    public boolean isOutOfBounds(int index){
+        return index < 0 || index >= gridCells.length;
+    }
 
-  public void addEntity(int e, int index){
-    getEntities(index).add(e);
-  }
+    public int getCellIndex(int x, int y){
+        if(isOutOfBounds(x, y)) {
+            return -1;
+        }
+        return x + y * width;
+    }
 
-  public void removeEntity(int e, int index){
-    getEntities(index).removeValue(e);
-  }
+    public int getCellIndex(Vector2 position){
+        return getCellIndex((int) (position.x / cellSize),(int) (position.y / cellSize));
+    }
 
-  public int getCellSize(){
-    return cellSize;
-  }
+    public IntBag getEntities(int index) {
+        try{
+            return this.gridCells[index];
+        }catch(IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    public IntBag getEntities(int x, int y) {
+        return getEntities(getCellIndex(x, y));
+    }
+
+    public void addEntity(int e, int index){
+        getEntities(index).add(e);
+    }
+
+    public void removeEntity(int e, int index){
+        getEntities(index).removeValue(e);
+    }
+
+    public int getCellSize(){
+        return cellSize;
+    }
  }
