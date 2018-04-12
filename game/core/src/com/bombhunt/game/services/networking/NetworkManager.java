5337ff228df9389e1d130e2f59db4fd3dc62913e
Erling Ihlen <erling_pondus@hotmail.com>
2018-04-12 15:39:36 +0200

    added setsender to the listener in the network manager

--- game/core/src/com/bombhunt/game/services/networking/NetworkManager.java
+++ game/core/src/com/bombhunt/game/services/networking/NetworkManager.java
@@ -6,8 +6,13 @@ public class NetworkManager implements RealtimeListener{
     private HashMap<Integer, RealtimeListener> listeners;
     private IPlayServices sender;
 
+    public NetworkManager(){
+        listeners = new HashMap<Integer, RealtimeListener>(255);
+    }
+
     public void openChannel(RealtimeListener listener, Integer channel) {
         listeners.put(channel, listener);
+        listener.setSender(new ChanneledSender(sender, channel));
     }
 
     @Override
