c9aa20ad14c9c12aea9ea20acb430673534e42ed
Aborysa <aborysa@gmail.com>
2018-04-12 15:35:33 +0200

    Created a sender that sends on a specific channel

---
+++ game/core/src/com/bombhunt/game/services/networking/ChanneledSender.java
@@ -0,0 +1,38 @@
+package com.bombhunt.game.services.networking;
+
+import java.io.Console;
+import java.util.Arrays;
+
+import com.artemis.injection.CachedClass;
+
+/**
+ * Created by erlin on 22.03.2018.
+ */
+
+public class ChanneledSender {
+    private IPlayServices playServices;
+    private int channel;
+
+    public ChanneledSender(IPlayServices playService, int channel){
+        this.playServices = playService;
+        this.channel = channel;
+    }
+    public void signIn(){this.playServices.signIn();}
+    public void signOut(){this.playServices.signOut();}
+    public void startMatchMaking(){this.playServices.startMatchMaking();}
+    public boolean isSignedIn(){return this.playServices.isSignedIn();}
+
+    private byte[] appendChannel(byte[] message){
+        byte[] nMessage = new byte[message.length + 1];
+        nMessage[0] = (byte)(this.channel & 0xFF);
+        System.arraycopy(message, 0, nMessage, 1, message.length);
+        return nMessage;
+    }
+
+    public void sendToAllReliably(byte[] message){
+        this.playServices.sendToAllReliably(appendChannel(message));
+    }
+
+    public void setRealTimeListener(RealtimeListener listener){
+    }
+}
