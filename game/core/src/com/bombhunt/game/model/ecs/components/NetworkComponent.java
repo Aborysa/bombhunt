125cf8b14119cfdf8b5322ee2be8de5b028211c0
Aborysa <aborysa@gmail.com>
2018-04-12 16:05:58 +0200

    Started working on a network component and system

---
+++ game/core/src/com/bombhunt/game/model/ecs/components/NetworkComponent.java
@@ -0,0 +1,12 @@
+package com.bombhunt.game.model.ecs.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.math.Vector2;
+import com.bombhunt.game.model.Grid;
+
+
+public class NetworkComponent extends Component {
+  public String owner = null;
+  public int turn = 0;
+  public int netId = -1;
+}
---
+++ game/core/src/com/bombhunt/game/model/ecs/systems/NetworkSystem.java
@@ -0,0 +1,67 @@
+package com.bombhunt.game.model.ecs.systems;
+
+import java.util.HashMap;
+
+import com.artemis.Aspect;
+import com.artemis.BaseEntitySystem;
+import com.artemis.ComponentMapper;
+import com.artemis.systems.IteratingSystem;
+import com.artemis.utils.IntBag;
+import com.bombhunt.game.model.ecs.components.Box2dComponent;
+import com.bombhunt.game.model.ecs.components.ExplosionComponent;
+import com.bombhunt.game.model.ecs.components.NetworkComponent;
+import com.bombhunt.game.model.ecs.components.TransformComponent;
+import com.bombhunt.game.services.networking.IPlayServices;
+import com.bombhunt.game.services.networking.Message;
+import com.bombhunt.game.services.networking.NetworkManager;
+import com.bombhunt.game.services.networking.RealtimeListener;
+
+public class NetworkSystem extends BaseEntitySystem implements RealtimeListener {
+    private ComponentMapper<ExplosionComponent> mapExplosion;
+    private ComponentMapper<TransformComponent> mapTransform;
+    private ComponentMapper<NetworkComponent> mapNetwork;
+    private ComponentMapper<Box2dComponent> mapBox2d;
+    private NetworkManager netManager;
+    private IPlayServices playServices;
+
+
+    private HashMap<Integer, Integer> entityIdMap = new HashMap<Integer, Integer>();
+
+    private int localTurn = 0;
+
+    public NetworkSystem(NetworkManager manager) {
+        super(
+          Aspect.all(NetworkComponent.class).one(TransformComponent.class, ExplosionComponent.class, Box2dComponent.class )
+        );
+        this.netManager = manager;
+        this.netManager.openChannel(this, 50);
+    }
+
+    @Override
+    protected void inserted(int entityId) {
+      super.inserted(entityId);
+      NetworkComponent netComponent = mapNetwork.get(entityId);
+      entityIdMap.put(entityId, netComponent.netId);
+    }
+
+    @Override
+    protected void removed(int entityId) {
+      super.removed(entityId);
+      entityIdMap.remove(entityId);
+    }
+
+    @Override
+    protected void processSystem() {
+      IntBag entities = subscription.getEntities();
+      localTurn++;
+    }
+
+    public void handleDataReceived(Message message){
+
+    }
+
+
+    public void setSender(IPlayServices playService){
+      this.playServices = playService;
+    }
+}
