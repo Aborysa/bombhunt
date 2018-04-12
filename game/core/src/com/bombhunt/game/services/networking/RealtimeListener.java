cf57a52ed3267c61eb68e698054e1dcc3c195095
Erling Ihlen <erling_pondus@hotmail.com>
2018-04-12 15:12:16 +0200

    created Message methods for recieving data out of the Message object, basic methods in networkmanager

--- game/core/src/com/bombhunt/game/services/networking/RealtimeListener.java
+++ game/core/src/com/bombhunt/game/services/networking/RealtimeListener.java
@@ -5,4 +5,5 @@ import java.util.EventListener;
 
 public interface RealtimeListener {
     public void handleDataRecieved(Message message);
+    public void setSender(IPlayServices playServices);
 }
