94e139d81ebafcbb6a41b48f749298025ed924c4
Aborysa <aborysa@gmail.com>
2018-04-12 15:16:41 +0200

    Fixed typo

--- game/core/src/com/bombhunt/game/services/networking/RealtimeListener.java
+++ game/core/src/com/bombhunt/game/services/networking/RealtimeListener.java
@@ -4,6 +4,6 @@ package com.bombhunt.game.services.networking;
 import java.util.EventListener;
 
 public interface RealtimeListener {
-    public void handleDataRecieved(Message message);
+    public void handleDataReceived(Message message);
     public void setSender(IPlayServices playServices);
 }
