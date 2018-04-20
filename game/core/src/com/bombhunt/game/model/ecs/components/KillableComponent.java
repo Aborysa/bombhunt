9b6cdd4de7b0bf1647affbacda64f43e7582f815
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-04-20 17:26:09 +0200

    tweaking persistency color

--- game/core/src/com/bombhunt/game/model/ecs/components/KillableComponent.java
+++ game/core/src/com/bombhunt/game/model/ecs/components/KillableComponent.java
@@ -8,6 +8,6 @@ public class KillableComponent extends Component {
     public float timer_damage = 0.2f;
     public float ttl_timer = timer_damage;
     public boolean is_colored = false;
-    public float color_persistence = 0.2f;
+    public float color_persistence = 0.4f;
     public float ttl_color = color_persistence;
 }
